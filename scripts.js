ab1589b86aa2f7a6a6db639f05c853c90d7153c5
devsfernandol <devsfernandol@gmail.com>
2024-05-27 22:31:13 -0400

    Version 0.1

---
+++ scripts.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const fadeInElements = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in-visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    fadeInElements.forEach(element => {
+        observer.observe(element);
+    });
+});
